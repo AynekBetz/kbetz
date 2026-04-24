2546abfbcfae96ef93f1e15f0d70d550777aeb9e
Kenya Dixon <yourgithubemail@example.com>
2026-04-23 23:30:04 -0400

    add homepage route fix

---
+++ frontend/app/page.js
@@ -0,0 +1,30 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Home() {
+useEffect(() => {
+const token = localStorage.getItem("token");
+
+```
+if (token) {
+  window.location.href = "/dashboard";
+} else {
+  window.location.href = "/login";
+}
+```
+
+}, []);
+
+return (
+<div style={{
+height: "100vh",
+background: "#050505",
+color: "white",
+display: "flex",
+justifyContent: "center",
+alignItems: "center"
+}}>
+Loading... </div>
+);
+}
