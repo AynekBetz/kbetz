a1951fd72ff7f1dab320d87d41a34fb04fbf1040
Kenya Dixon <yourgithubemail@example.com>
2026-05-13 09:37:52 -0400

    connect home to dashboard

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,3 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <div style={{color: "white"}}>HOME WORKS</div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    // redirect instantly to dashboard
+    router.push("/dashboard");
+  }, []);
+
+  return (
+    <div style={{ color: "white", padding: "20px" }}>
+      Loading KBETZ...
+    </div>
+  );
 }
